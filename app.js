057d1ac12b34b5abe4d8584e3af3b5387f36995a
Robert JF <robertojf2012@gmail.com>
2018-11-26 21:25:59 -0700

    pantalla de registro lista para API

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ const api = require('./routes/articulo');
 
 app.use(express.static(__dirname + '/public/'))
 
+//Agregado para poder tener permiso a la hora de hacer las peticiones GET, POST
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/api', api)
---
+++ public/js/blog.js
@@ -0,0 +1,34 @@
+
+function getArticulos(){
+
+}
+
+
+//FORMAT DATE
+/*
+function formatDate(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
+*/
+
+/*
+var content = '';
+content +='<tr>';
+content += '<td>'+'<img style="max-width:100px; max-height: 100px;" src="'+ snapshot2.child("imagen").val() +'"'+'>'+'</td>';
+content += '<td>'+ snapshot2.child("titulo").val() +'</td>';
+content += '<td>'+ fecha +'</td>';
+content += '<td>'+ '<a onclick="editarNoticia(\''+fraccionamiento+'/noticias/'+snapshot2.key+'/'+'\')" class="btn green-haze btn-outline btn-circle btn-sm" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">'+ 'Editar' +'</a>' + '<a onclick="borrarNoticia(\''+fraccionamiento+'/noticias/'+snapshot2.key+'/'+'\',\''+snapshot2.child("imagen").val()+'\')" class="btn red-haze btn-outline btn-circle btn-sm" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">'+ 'Borrar' +'</a>' +'</td>';
+content +='</tr>';
+$('#tablaNoticias').append(content);
+*/
