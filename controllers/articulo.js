534afc5f31f4733bc727e4f69d3666bbc5b04018
ivanmalilla36 <ivanmalilla36@gmail.com>
2018-11-27 15:27:09 -0700

    cambios que no valen verga

--- controllers/articulo.js
+++ controllers/articulo.js
@@ -2,8 +2,20 @@
 
 const Articulo = require('../models/articulo');
 
+function getArticulos(res,req){
+	Articulo.find({}).collation({locale:"en"}).exec((err,articulos)=>{
+    if(err){
+      return res.status(500).send();
+    }
+    if(!articulos){
+      return res.status(404).send();
+    }
 
+    return res.status(200).send(articulos);
+
+  });
+}
 
 module.exports = {
-
+	getArticulos
 }
--- routes/articulo.js
+++ routes/articulo.js
@@ -4,9 +4,6 @@ const express = require('express');
 const ArticuloController = require('../controllers/articulo');
 const api = express.Router();
 
-
-
-
-
+api.get('/articulos', ArticuloController.getArticulos)
 
 module.exports = api;
